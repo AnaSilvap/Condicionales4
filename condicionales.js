147efcfbc73135927119a1941193a4763d12844b
AnaSilvap <92560459+AnaSilvap@users.noreply.github.com>
2021-11-29 21:05:14 -0500

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,13 @@
+number = Number(prompt("ingrese su número"))
+if (Number. isInteger(number/ 15))
+Respuesta = "FizzBuzz"
+else
+    if (Number. isInteger(number/ 5)) 
+    Respuesta ="Buzz"
+else
+    if (Number. isInteger)((number/ 3))
+    Respuesta = "Fizz"
+
+document.getElementById("Texto").innerHTML=Respuesta
+
+        
